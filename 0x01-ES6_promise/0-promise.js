f78b24bfba69a8e74919f195022c8c6c1e02fde3
kase2228 <etelethiopia@gmail.com>
2024-10-02 21:21:29 +0300

    test

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+function getResponseFromAPI() {
+    return new promise((resolve, reject) => {
+        if (x = 0) {
+            resolve("0");
+        } else {
+            reject("1");
+        }
+    });
+}
