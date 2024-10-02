39866b0e907fdb4973bc287ebc263f81662a43ce
kase2228 <etelethiopia@gmail.com>
2024-10-02 23:08:44 +0300

    returning a promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,9 @@
 function getResponseFromAPI() {
-    return (new Promise((resolve, reject) => {}));
+    return (new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Success!');
+          } else {
+            reject('Failure');
+          }
+    }));
 }
