ac44aa2765a1ed535f1cd3d6e2d9889b0e63c767
kase2228 <etelethiopia@gmail.com>
2024-10-03 22:34:27 +0300

    abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -10,6 +10,9 @@ export default class Building{
     get sqft() {
         return this._sqft;
     }
+    set sqft(value) {
+        this._sqft = value;
+    }
 
     evacuationWarningMessage() {
         throw new Error("Class extending Building must override evacuationWarningMessage");
