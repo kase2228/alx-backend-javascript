b933895a8e19142ca286882852dbeabf9d93289b
kase2228 <etelethiopia@gmail.com>
2024-10-03 15:43:39 +0000

    creating classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,15 +1,5 @@
-import ClassRoom from './0-classroom.js'
+import ClassRoom from './0-classroom';
 
-<<<<<<< HEAD
-export default function* initializeRooms() {
-    yield ClassRoom(19)
-    yield ClassRoom(20)
-    yield ClassRoom(34)
+export default function initializeRooms() {
+  return ([new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]);
 }
-=======
-function* initializeRooms() {
-    yield new ClassRoom(19);
-    yield new ClassRoom(20);
-    yield new ClassRoom(34);
-}
->>>>>>> 5685833e37f8a1dbad4354da7b8a59cb76673ebe
