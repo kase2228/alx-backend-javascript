5a85f63f571d2674a7006f3a18f291b28e4d4c05
kase2228 <etelethiopia@gmail.com>
2024-10-03 22:50:12 +0300

    overriding

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,15 @@
+import Building from "./5-building";
+
+export default class SkyHighBuilding extends Building {
+    constructor(sqft, floors) {
+        super(sqft);
+        this._floors = floors;
+    }
+
+    get floors() {
+        return this._floors;
+    }
+    evacuationWarningMessage() {
+        return `Evacuate slowly the ${floors} floors`
+    }
+}
