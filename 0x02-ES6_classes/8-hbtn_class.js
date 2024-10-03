6923dd857910f441dafa9afc3ca7aed293e445fd
kase2228 <etelethiopia@gmail.com>
2024-10-03 23:31:10 +0300

    overriding3

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,13 @@
+export default class HolbertonClass {
+    constructor(size, location) {
+        this._size = size;
+        this._location = location;
+    }
+
+    Number(Size) {
+        this._size = Size;
+    }
+    String(Location) {
+        this._location = Location;
+    }
+}
