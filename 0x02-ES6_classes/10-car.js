2a6bee5f6561d5e6cbff99defae0689293668dec
kase2228 <etelethiopia@gmail.com>
2024-10-04 00:21:04 +0300

    new instance

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -6,6 +6,6 @@ export default class Car {
     }
 
     cloneCar() {
-        return (new Car(this._brand, this._motor, this._color));
+        return (this.constructor(this._brand, this._motor, this._color));
     }
 }
