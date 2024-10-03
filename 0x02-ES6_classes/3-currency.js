8db59f9971735e60cc0beed222db1574b938d2c0
kase2228 <etelethiopia@gmail.com>
2024-10-03 20:06:36 +0300

    method

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+    constructor(code, name) {
+        this._code = code;
+        this._name = name;
+    }
+
+    get code() {
+        return this._code;
+    }
+    set code(Code) {
+        this._code = Code;
+    }
+
+    get name() {
+        return this._name;
+    }
+    set name(Name) {
+        this._name = Name;
+    }
+
+    displayFullCurrency() {
+        return console.log(
+            `
+            ${this._name} (${this._code})
+            `
+        );
+    }
+}
