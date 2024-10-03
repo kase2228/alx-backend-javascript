5685833e37f8a1dbad4354da7b8a59cb76673ebe
kase2228 <etelethiopia@gmail.com>
2024-10-03 18:07:02 +0300

    initializing rooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 import ClassRoom from './0-classroom.js'
 
 function* initializeRooms() {
-    yield ClassRoom(19)
-    yield ClassRoom(20)
-    yield ClassRoom(34)
+    yield new ClassRoom(19);
+    yield new ClassRoom(20);
+    yield new ClassRoom(34);
 }
