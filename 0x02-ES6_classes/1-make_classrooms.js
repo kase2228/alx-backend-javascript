9f2fdf032db84e4cb556e1516c80a991b09163b1
kase2228 <etelethiopia@gmail.com>
2024-10-03 15:08:10 +0000

    task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from './0-classroom.js'
 
-function* initializeRooms() {
+export default function* initializeRooms() {
     yield ClassRoom(19)
     yield ClassRoom(20)
     yield ClassRoom(34)
