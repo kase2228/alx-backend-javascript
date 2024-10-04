049eafb45d6b92a6c7d0db781ef363caa213ed13
kase2228 <etelethiopia@gmail.com>
2024-10-04 23:20:31 +0300

    utils

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,6 +4,9 @@ export default function handleProfileSignup() {
     return new Promise.all([uploadPhoto(), createUser])
      .then((results) => {
         const [photo, user] = results;
-        console.log(`${photo.photo} ${user.firstname} ${user.lastname}`)
+        console.log(`${photo.photo} ${user.firstname} ${user.lastname}`);
+    }).catch(() => {
+        console.log('Signup system offline');
     });
 }
+
