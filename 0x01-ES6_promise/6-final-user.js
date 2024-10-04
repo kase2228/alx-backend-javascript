c02e97b2e381136f511f71c31ececc4eed8ff372
kase2228 <etelethiopia@gmail.com>
2024-10-04 23:48:40 +0300

    calling funs

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+        .then((results) => {
+            return results.map((result) => {
+                if (result.status === 'fulfilled') {
+                    return { status: 'fulfilled', value: result.value };
+                } else {
+                    return { status: 'rejected', value: result.reason };
+                }
+            });
+        });
+}
