361c65da0a79880e07e528367113c1c90c015d01
kase2228 <etelethiopia@gmail.com>
2024-10-04 23:17:47 +0300

    utils

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default function handleProfileSignup() {
+    return new Promise.all([uploadPhoto(), createUser])
+     .then((results) => {
+        const [photo, user] = results;
+        console.log(`${photo.photo} ${user.firstname} ${user.lastname}`)
+    });
+}
