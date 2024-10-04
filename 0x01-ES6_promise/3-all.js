6a72019bf4d1d54d77da56973f5293554c5c70ba
kase2228 <etelethiopia@gmail.com>
2024-10-04 23:24:11 +0300

    utils

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
 import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup() {
-    return new Promise.all([uploadPhoto(), createUser])
+    return Promise.all([uploadPhoto(), createUser()])
      .then((results) => {
         const [photo, user] = results;
-        console.log(`${photo.photo} ${user.firstname} ${user.lastname}`);
+        console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     }).catch(() => {
         console.log('Signup system offline');
     });
